02ad0894c1f88f1fbd13f3f551cda7c1d11e742f
sashikanth <sashirubik@gmail.com>
2017-12-23 01:52:31 +0530

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	# we are using 'forms.ModelForm' because if we use 'forms.Form' we
+	# again need to write FormFields similar to fields in models.py
+	# In order to get rid of that we use META class where we mention
+	# we use fields from Model-Post
+	class Meta:
+		model = Post
+		fields = ('title', 'text', )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone # To get timestamp 
 from .models import Post # Import Post model. That's where the data is.
+from .forms import PostForm 
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,34 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post,})
+
+# for adding new Post
+def post_new(request):
+
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+
+	return render(request, 'blog/post_edit.html', {'form': form,})
+
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
