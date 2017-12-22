d840a9aa1c1ec27aa516ad05b00aa1cb71cf0490
sashikanth <sashirubik@gmail.com>
2017-12-23 00:44:21 +0530

    Added CSS to template and produced detailed posts link

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone # To get timestamp 
 from .models import Post # Import Post model. That's where the data is.
 
@@ -6,3 +6,9 @@ from .models import Post # Import Post model. That's where the data is.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts,})
+
+# for detailed post
+def post_detail(request, pk):
+	# post = Post.objects.get(pk=pk)
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post,})
